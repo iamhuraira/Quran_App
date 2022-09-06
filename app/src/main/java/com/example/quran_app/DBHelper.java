4c7abe6b8e1b2d120f8e0d82495b2daea09e5f1d
Abu Huraira <iamhuraira429@gmail.com>
2022-09-06 09:30:46 +0500

    Checking Database exists or not

--- app/src/main/java/com/example/quran_app/DBHelper.java
+++ app/src/main/java/com/example/quran_app/DBHelper.java
@@ -1,4 +1,33 @@
 package com.example.quran_app;
 
-public class DBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.File;
+
+public class DBHelper  extends SQLiteOpenHelper {
+    Context context;
+    String DB_PATH;
+    String divider = "/";
+    String DB_NAME = "quran_app.db";
+
+    public  DBHelper(Context context){
+        super(context, "quran_app.db", null, 1);
+        this.context = context;
+        DB_PATH = divider + "data" + divider + "data" + divider + context.getPackageName() + divider + "databases/";
+    }
+    @Override
+    public void onCreate(SQLiteDatabase arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+    }
+    public  boolean isDataBaseExists(){
+        File dbFile = new File(DB_PATH + DB_NAME);
+        return dbFile.exists();
+    }
 }
